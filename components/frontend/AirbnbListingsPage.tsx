d7d2b691c54a38ee82a18902b3146cd9b173638c
kooza-colline <koozacollinz1@gmail.com>
2025-03-25 15:30:49 +0300

    removed unused value

--- components/frontend/AirbnbListingsPage.tsx
+++ components/frontend/AirbnbListingsPage.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Suspense, useState } from "react";
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
 import { useProperties } from "@/hooks/useProperty";
